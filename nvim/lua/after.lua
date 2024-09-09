85844c035c880b0a3264f7c4991e44863ba70666
Erik <eturnq@gmail.com>
2024-09-08 22:09:09 -0500

    Neovim config.

--- nvim/lua/after.lua
+++
@@ -1,3 +0,0 @@
-require "configs.treesitter"
-
-require "mappings"
--- nvim/lua/mappings.lua
+++
@@ -1,4 +0,0 @@
--- Set leader to " " if it isn't already
-if vim.g.mapleader ~= " " then vim.g.mapleader = " " end
-
-vim.keymap.set("n", "<leader>e", ":NERDTreeToggle<CR>")
