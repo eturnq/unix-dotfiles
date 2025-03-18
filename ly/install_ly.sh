203edd092a818b065d3ef2926ad6a8de47ef4ca5
Erik <eturnq@gmail.com>
2025-03-18 14:36:54 -0500

    Determine init system by checking package manager in install_ly.sh.

--- ly/install_ly.sh
+++ ly/install_ly.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 USER=$(id -un)
+PKG=$(python3 -c "from py_mod/install_dep import get_package_manager ; print(get_package_manager())")
+if [ $PKG -eq "apt-get" ] ; then
+	INITSVC=""
+elif [ $PKG -eq "xbps-install" ] ; then
+	INITSRV="-DINIT_SYSTEM=runit"
+else ;
+	INITSRV=""
+fi
 
 sudo mkdir -p /usr/src/ly
 sudo chown $USER:$USER /usr/src/ly
@@ -8,6 +16,6 @@ git clone https://codeberg.org/AnErrupTion/ly /usr/src/ly
 
 pushd /usr/src/ly
 	git pull
-	$HOME/.local/bin/zig build
+	$HOME/.local/bin/zig build $INISRV
 	sudo $HOME/.local/bin/zig build installexe
 popd
