586201ab81a842e37d634996f9842c331de78d41
Erik <eturnq@gmail.com>
2025-03-18 15:14:33 -0500

    Bugfix: syntax of fvwm/setup.sh.

--- fvwm/setup.sh
+++ fvwm/setup.sh
@@ -6,7 +6,7 @@ pushd /tmp
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Noto.zip
 popd
 
-mkdir -p $(HOME)/.fonts
-unzip /tmp/Noto.zip -d $(HOME)/.fonts
+mkdir -p $HOME/.fonts
+unzip /tmp/Noto.zip -d $HOME/.fonts
 rm /tmp/Noto.zip
 fc-cache
