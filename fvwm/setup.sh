fe54026953dca27d00f5bd939dc0b5d94fd97910
Erik <eturnq@gmail.com>
2025-03-19 16:01:32 -0500

    Bugfix: set update-alternatives kludge as executable.

--- fvwm/setup.sh
+++ fvwm/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+chmod +x $HOME/.local/bin/update-alternatives
 sudo env -S PATH=$PATH:/$HOME/.local/bin update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which xterm) 20
 
 pushd /tmp
