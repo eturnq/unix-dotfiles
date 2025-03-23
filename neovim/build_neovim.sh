645a4493c3e7d4ca235796c7a68b355412c12906
Erik <eturnq@gmail.com>
2025-03-22 21:02:25 -0500

    Check for gmake in build_neovim.sh.

--- neovim/build_neovim.sh
+++ neovim/build_neovim.sh
@@ -3,13 +3,15 @@
 USER=$(id -un)
 su - $USER -c "nvm install --lts" # NPM is required for some Neovim packages.
 
+[[ $(type -p "gmake") ]] && MAKE="gmake" || MAKE="make" # Use gmake if it is installed, otherwise make
+
 pushd /usr/src
 	sudo mkdir neovim
 	sudo chown $USER:$USER neovim
 	git clone https://github.com/neovim/neovim
 	
 	pushd neovim
-		gmake CMAKE_BUILD_TYPE=RelWithDebInfo
-		sudo gmake install
+		$(MAKE) CMAKE_BUILD_TYPE=RelWithDebInfo
+		sudo $(MAKE) install
 	popd
 popd
